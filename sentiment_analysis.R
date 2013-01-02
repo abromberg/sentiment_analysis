09801aa1aa0dfffb23431c2096e1155fb6eb116d
abromberg <abromberg@stanford.edu>
2013-01-02 03:19:34 -0500

    v1.4.1
    Commented everything!

--- sentiment_analysis.R
+++ sentiment_analysis.R
@@ -1,18 +1,22 @@
+#import libraries to work with
 library(plyr)
 library(stringr)
 library(e1071)
 
 setwd("~/Documents/GitHub/sentiment_analysis")
 
+#load up word polarity list and format it
 afinn_list <- read.delim(file='AFINN/AFINN-111.txt', header=FALSE, stringsAsFactors=FALSE)
 names(afinn_list) <- c('word', 'score')
 afinn_list$word <- tolower(afinn_list$word)
 
+#categorize words as very negative to very positive and add some movie-specific words
 vNegTerms <- afinn_list$word[afinn_list$score==-5 | afinn_list$score==-4]
 negTerms <- c(afinn_list$word[afinn_list$score==-3 | afinn_list$score==-2 | afinn_list$score==-1], "second-rate", "moronic", "third-rate", "flawed", "juvenile", "boring", "distasteful", "ordinary", "disgusting", "senseless", "static", "brutal", "confused", "disappointing", "bloody", "silly", "tired", "predictable", "stupid", "uninteresting", "trite", "uneven", "outdated", "dreadful", "bland")
 posTerms <- c(afinn_list$word[afinn_list$score==3 | afinn_list$score==2 | afinn_list$score==1], "first-rate", "insightful", "clever", "charming", "comical", "charismatic", "enjoyable", "absorbing", "sensitive", "intriguing", "powerful", "pleasant", "surprising", "thought-provoking", "imaginative", "unpretentious")
 vPosTerms <- c(afinn_list$word[afinn_list$score==5 | afinn_list$score==4], "uproarious", "riveting", "fascinating", "dazzling", "legendary")
 
+#load up positive and negative sentences and format
 posText <- read.delim(file='polarityData/rt-polaritydata/rt-polarity-pos.txt', header=FALSE, stringsAsFactors=FALSE)
 posText <- posText$V1
 posText <- unlist(lapply(posText, function(x) { str_split(x, "\n") }))
@@ -20,26 +24,30 @@ negText <- read.delim(file='polarityData/rt-polaritydata/rt-polarity-neg.txt', h
 negText <- negText$V1
 negText <- unlist(lapply(negText, function(x) { str_split(x, "\n") }))
 
-
+#function to calculate number of words in each category within a sentence
 sentimentScore <- function(sentences, vNegTerms, negTerms, posTerms, vPosTerms){
   final_scores <- matrix('', 0, 5)
   scores <- laply(sentences, function(sentence, vNegTerms, negTerms, posTerms, vPosTerms){
     initial_sentence <- sentence
+    #remove unnecessary characters and split up by word 
     sentence <- gsub('[[:punct:]]', '', sentence)
     sentence <- gsub('[[:cntrl:]]', '', sentence)
     sentence <- gsub('\\d+', '', sentence)
     sentence <- tolower(sentence)
     wordList <- str_split(sentence, '\\s+')
     words <- unlist(wordList)
+    #build vector with matches between sentence and each category
     vPosMatches <- match(words, vPosTerms)
     posMatches <- match(words, posTerms)
     vNegMatches <- match(words, vNegTerms)
     negMatches <- match(words, negTerms)
+    #sum up number of words in each category
     vPosMatches <- sum(!is.na(vPosMatches))
     posMatches <- sum(!is.na(posMatches))
     vNegMatches <- sum(!is.na(vNegMatches))
     negMatches <- sum(!is.na(negMatches))
     score <- c(vNegMatches, negMatches, posMatches, vPosMatches)
+    #add row to scores table
     newrow <- c(initial_sentence, score)
     final_scores <- rbind(final_scores, newrow)
     return(final_scores)
@@ -47,29 +55,23 @@ sentimentScore <- function(sentences, vNegTerms, negTerms, posTerms, vPosTerms){
   return(scores)
 }
 
+#build tables of positive and negative sentences with scores
 posResult <- as.data.frame(sentimentScore(posText, vNegTerms, negTerms, posTerms, vPosTerms))
 negResult <- as.data.frame(sentimentScore(negText, vNegTerms, negTerms, posTerms, vPosTerms))
-
 posResult <- cbind(posResult, 'positive')
 colnames(posResult) <- c('sentence', 'vNeg', 'neg', 'pos', 'vPos', 'sentiment')
 negResult <- cbind(negResult, 'negative')
 colnames(negResult) <- c('sentence', 'vNeg', 'neg', 'pos', 'vPos', 'sentiment')
 
+#combine the positive and negative tables
 results <- rbind(posResult, negResult)
+
+#run the naive bayes algorithm using all four categories
 classifier <- naiveBayes(results[,2:5], results[,6])
 
+#display the confusion table for the classiciation ran on the same data
 confTable <- table(predict(classifier, results), results[,6], dnn=list('predicted','actual'))
-
 confTable
 
+#run a binomial test for confidence interval of results
 binom.test(confTable[1,1] + confTable[2,2], nrow(results), p=0.5)
-
-####COMMENT
-
-#resources:
-#inspiration and initial code: http://www.inside-r.org/howto/mining-twitter-airline-consumer-sentiment
-#inspiration, recommended AFINN: http://viksalgorithms.blogspot.com.au/2012/06/tracking-us-sentiments-over-time-in.html
-#word list (AFINN): http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010
-#additional movie description words: http://member.tokoha-u.ac.jp/~dixonfdm/Writing%20Topics%20htm/Movie%20Review%20Folder/movie_descrip_vocab.htm
-#idea for bayes for classification rather than just adding and subtracting: http://danzambonini.com/self-improving-bayesian-sentiment-analysis-for-twitter/
-#sentence polarity dataset: http://www.cs.cornell.edu/people/pabo/movie-review-data/
